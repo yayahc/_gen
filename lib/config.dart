a530c4adeeae654c0adb3d9d0b6d38e49a310003
yayahc <hamedcuenca5@gmail.com>
2024-02-04 22:51:07 +0000

    move from .env config to schema direct config

--- lib/config.dart
+++ lib/config.dart
@@ -11,16 +11,7 @@ Future<PrismaClient> getPrismaClient() async {
 
 /// Setup .env file for prisma client
 /// Check if there is a .env file if not create one with required params
-Future setupEnv() async {
-  final file = File("../.env");
-  final rs = await checkExistingEnv(file);
-  if (!rs) {
-    print("object");
-    await createEnv(file);
-  }
-}
-
-Future<void> createEnv(File file,
+Future<void> setupUrl(
     {String? provider,
     String? username,
     String? password,
@@ -28,18 +19,11 @@ Future<void> createEnv(File file,
     String? dbPort,
     String? dbName,
     String? schema = "public"}) async {
-  await file.create();
-  await file.writeAsString(
-      """DATABASE_URL="$provider://$username:$password@$localHost:$dbPort/$dbName?schema=$schema""");
-}
+  final file = File("../prisma/schema.prisma");
 
-Future<bool> checkExistingEnv(File file) async {
-  return await file.exists();
-}
-
-Future<String> getEnv() async {
-  final file = File("../.env");
-  final content =
-      (await checkExistingEnv(file)) ? await file.readAsString() : "";
-  return content;
+  final lines = await file.readAsLines();
+  final index = lines.indexWhere((line) => line.contains("url"));
+  lines[index] =
+      """  url      = "$provider://$username:$password@$localHost:$dbPort/$dbName?schema=$schema" """;
+  await file.writeAsString(lines.join('\n'));
 }
